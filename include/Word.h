0fb7a9858a0c40611dfb477bcc61492f1f0fd19e
SergioRosello <sergio-rosello@hotmail.com>
2019-10-18 12:07:47 +0200

    Added struct Vector, to operate with words

--- include/Word.h
+++ include/Word.h
@@ -1,11 +1,63 @@
 #include "../include/Letter.h"
 
-class Word {
-  private:
-    // A word can have as many letters as it wants
-    Coordinates *word;
+#ifndef PI
+#define PI 3.14159265
+#endif
 
-    // Constructor with one or more letters
-    Word(Letter);
+struct Vector {
+  float x;
+  float y;
+  double magnitude;
+  float angle;
+
+  // Constructor
+  // Creates the vector given two coordinates
+  Vector(Coordinates x, Coordinates y)
+    : x(y.x - x.x),
+    y(y.y - x.x),
+    magnitude(getMagnitude()),
+    angle(getAngle())
+  {}
+
+  // Calculates the magnitude of the vector
+  double getMagnitude();
+
+  float getAngle();
+
+};
+
+class Word {
+  public: 
+
+    // We will construct the word based on its firs letter's coordinates
+    // and the last letter's coordinates.
+    // The reason for this is that the user will be able to create
+    // any word he wants, and the board will be the resopnsable
+    // to check if the word is valid or not
+    // We do have to check here that the word is possible
+    // There are 8 possible word directions.
+    Word(Letter, Letter);
+
+    int getSize(){
+      return size;
+    }
+
+  private:
+
+    // The coordinates for the first and last letter of the word
+    Letter first;
+    Letter last;
+
+    // We interpret a word as a vector.
+    // and calculate it's:
+    // Magnitude: Length of the line segment
+    // Direction: Angle the line forms
+    bool checkIfWordDirectionIsValid();
+
+    // We use the vector magnitude to check the word's length
+    float wordMagnitude();
+
+    // Integer to know the word's size
+    int size;
     ~Word();
 };
--- src/Word.cpp
+++ src/Word.cpp
@@ -1,4 +1,20 @@
+#include <cmath>
 #include "../include/Word.h"
 
-Word::Word(Letter letter) {
+Word::Word(Letter f, Letter l) {
+  this->first = f;
+  this->last = l;
+}
+
+// Calculates the magnitude of the vector
+double Vector::getMagnitude(){
+  return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+float Vector::getAngle() {
+  float tempAngle;
+  //angle = inv cosine of dot product divided the the product of the magnitude of vector a and vector b
+  tempAngle = acos(x/magnitude);
+  //return the angle in degrees
+  return tempAngle*180/PI;
 }
