c5b2ffe69f0af494693d5dc96cd5aa13644fd19b
minicatsCB <minicatsCB@users.noreply.github.com>
2019-10-22 01:29:17 +0200

    Implement User class

--- src/User.cpp
+++ src/User.cpp
@@ -1,5 +1,27 @@
 #include "../include/User.h"
+using namespace std;
 
-User::User(int number){
-  this->test = number;
+User::User(string username) {
+  this->username = username;
+  this->score = 0;
+}
+
+void User::incrementScore(int score){
+  this->score += score;
+}
+
+string User::getUsername() {
+  return this->username;
+}
+
+void User::setUsername(string username) {
+  this->username = username;
+}
+
+int User::getScore() {
+  return this->score;
+}
+
+void User::resetScore() {
+  this->score = 0;
 }
