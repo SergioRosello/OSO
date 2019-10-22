d0fddf2cb4f6ded6f9915882c058446728918f1c
SergioRosello <sergio-rosello@hotmail.com>
2019-10-22 08:34:07 +0200

    Changed to meet UML diagram specification

--- include/Word.h
+++ include/Word.h
@@ -1,41 +1,9 @@
 #include "../include/Letter.h"
 
-#ifndef PI
-#define PI 3.14159265
-#endif
-
-struct Vector {
-  float x;
-  float y;
-  double magnitude;
-  float angle;
-
-  // Constructor
-  // Creates the vector given two coordinates
-  Vector(Coordinates x, Coordinates y)
-    : x(y.x - x.x),
-    y(y.y - x.x),
-    magnitude(getMagnitude()),
-    angle(getAngle())
-  {}
-
-  // Calculates the magnitude of the vector
-  double getMagnitude();
-
-  float getAngle();
-
-};
-
 class Word {
   public: 
 
-    // We will construct the word based on its firs letter's coordinates
-    // and the last letter's coordinates.
-    // The reason for this is that the user will be able to create
-    // any word he wants, and the board will be the resopnsable
-    // to check if the word is valid or not
-    // We do have to check here that the word is possible
-    // There are 8 possible word directions.
+    // Parameters are first and last letter from the word
     Word(Letter, Letter);
 
     int getSize(){
@@ -48,16 +16,7 @@ class Word {
     Letter first;
     Letter last;
 
-    // We interpret a word as a vector.
-    // and calculate it's:
-    // Magnitude: Length of the line segment
-    // Direction: Angle the line forms
-    bool checkIfWordDirectionIsValid();
-
-    // We use the vector magnitude to check the word's length
-    float wordMagnitude();
-
-    // Integer to know the word's size
+    // Stores the word size
     int size;
     ~Word();
 };
--- src/Word.cpp
+++ src/Word.cpp
@@ -1,20 +1,10 @@
 #include <cmath>
 #include "../include/Word.h"
 
+// Check if the word is valid
+// initialise the word
 Word::Word(Letter f, Letter l) {
   this->first = f;
   this->last = l;
 }
 
-// Calculates the magnitude of the vector
-double Vector::getMagnitude(){
-  return sqrt(pow(x, 2) + pow(y, 2));
-}
-
-float Vector::getAngle() {
-  float tempAngle;
-  //angle = inv cosine of dot product divided the the product of the magnitude of vector a and vector b
-  tempAngle = acos(x/magnitude);
-  //return the angle in degrees
-  return tempAngle*180/PI;
-}
