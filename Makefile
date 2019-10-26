eeea805eb7c543768f39562c2c4f23ea8521bc78
SergioRosello <sergio-rosello@hotmail.com>
2019-10-27 01:33:34 +0200

    Added word tests

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ OSO_CFLAGS = -o OSO
 TEST_CFLAGS = -o OSO -lgtest -lpthread
 CC = g++
 OSO_SRC = src/*.cpp
-TEST_SRC = tests/TestBoard.cpp tests/TestUser.cpp tests/main.cpp src/Board.cpp src/User.cpp
+TEST_SRC = tests/TestBoard.cpp tests/TestUser.cpp tests/TestWord.cpp tests/main.cpp src/Board.cpp src/User.cpp src/Word.cpp src/Letter.cpp
 OSO_OBJ = $(OSO_SRC:.cpp = .o)
 TEST_OBJ = $(TEST_SRC:.cpp = .o)
 oso:
--- src/Main.cpp
+++ src/Main.cpp
@@ -4,13 +4,10 @@
 
 int main(){
   Letter letter = Letter(Coordinates(20, 11), 'O');
-  Coordinates coordinates = letter.getCoordinates();
 
   Letter secondLetter = Letter(Coordinates(21, 12), 'S');
-  Coordinates secondCoordinates = letter.getCoordinates();
 
   Letter thirdLetter = Letter(Coordinates(22, 13), 'O');
-  Coordinates thidrCoordinates = letter.getCoordinates();
 
   Word word = Word(letter, thirdLetter);
   std::cout << "Word size: " << word.getSize() << "\n";
