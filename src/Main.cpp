f3f4954efb4ff84661655fa935791cb436e2b7e1
minicatsCB <minicatsCB@users.noreply.github.com>
2020-05-24 20:20:53 +0200

    Add player management to game flow

--- src/Main.cpp
+++ src/Main.cpp
@@ -27,47 +27,66 @@ int getIntegerInput(string msg) {
 }
 
 int main() {
-    int rows = 10;
-    int columns = 10;
+    int rows = 6;
+    int columns = 6;
     Board board = Board(rows, columns, "oso");
+    vector<Player> players = { Player("Player1"), Player("Player2") };
+    int rounds = 3;
 
-    char exitKey;
-    do {
-        board.ShowBoard();
-
-        char newLetter;
-        int xCoord, yCoord;
-        cout << "Your turn! What letter do you want to put (o/s)" << endl;
-        cin >> newLetter;
-        xCoord = getIntegerInput("Please, enter X coord of letter: ");
-        yCoord = getIntegerInput("Please, enter Y coord of letter: ");
-        if (board.AreCoordinatesValid(xCoord, yCoord)) {
-            cout << "Coordinates are valid" << endl;
-            board.PlaceLetter(Coordinates(xCoord, yCoord), newLetter);
-            cout << "Do you see any OSO? (y/n)" << endl;
-            char isUserSeingKeyword;
-            cin >> isUserSeingKeyword;
-            if (isUserSeingKeyword == 'y') {
-                cout << "Mark it!" << endl;
-                vector<Cell> selectedLetters;
-                for (int i = 0; i < board.keyword().size(); i++) {
-                    cout << "What are the three coordinates of letter number "<< i << "?: " << endl;
-                    xCoord = getIntegerInput("Please, enter X coord of letter: ");
-                    yCoord = getIntegerInput("Please, enter Y coord of letter: ");
-                    Coordinates coords = Coordinates(xCoord, yCoord);
-                    char content = board.GetLetterAt(coords).content();
-                    Cell selectedLetter = Cell(coords, content);
-                    selectedLetters.push_back(selectedLetter);
+    bool isBadStep = false;
+    for (size_t i = 0; i < rounds; i++)
+    {
+        cout << "******* Round " << (i + 1) << " *******" << endl;
+        for (Player& p : players) {
+            cout << "Your turn, " << p.name() << "!" << endl;
+            do {
+                if (isBadStep) {
+                    cout << "Playing " << p.name() << endl;
+                    isBadStep = false;
                 }
+                
+                board.ShowBoard();
 
-                board.IsSelectionValid(selectedLetters);
-            }
-        } else {
-            cout << "Coordinates are NOT valid" << endl;
+                cout << "What letter do you want to put (o/s)" << endl;
+                char newLetter;
+                int xCoord, yCoord;
+                cin >> newLetter;
+                xCoord = getIntegerInput("Please, enter X coord of letter: ");
+                yCoord = getIntegerInput("Please, enter Y coord of letter: ");
+                if (board.AreCoordinatesValid(xCoord, yCoord)) {
+                    board.PlaceLetter(Coordinates(xCoord, yCoord), newLetter);
+                    board.ShowBoard();
+
+                    cout << "Do you see any OSO? (y/n)" << endl;
+                    char isUserSeingKeyword;
+                    cin >> isUserSeingKeyword;
+                    if (isUserSeingKeyword == 'y') {
+                        cout << "Mark it!" << endl;
+                        vector<Cell> selectedLetters;
+                        for (int i = 0; i < board.keyword().size(); i++) {
+                            cout << "What are the three coordinates of letter number "<< i << "?: " << endl;
+                            xCoord = getIntegerInput("Please, enter X coord of letter: ");
+                            yCoord = getIntegerInput("Please, enter Y coord of letter: ");
+                            Coordinates coords = Coordinates(xCoord, yCoord);
+                            char content = board.GetLetterAt(coords).content();
+                            Cell selectedLetter = Cell(coords, content);
+                            selectedLetters.push_back(selectedLetter);
+                        }
+
+                        if (board.IsSelectionValid(selectedLetters)) {
+                            cout << "+1 for " << p.name() << endl;
+                            p.IncrementScore(1);
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    cout << "Coordinates are NOT valid" << endl;
+                    isBadStep = true;
+                }
+            } while (isBadStep);
         }
-        cout << "Exit (y/n)?";
-        cin >> exitKey;
-    } while (exitKey != 'y');
+    }
 
     return 0;
 }
